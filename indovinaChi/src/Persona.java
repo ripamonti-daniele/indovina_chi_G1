055250972e9ae08b6f01420aa4d3bcb2af171995
DANIELE RIPAMONTI <ripamonti.daniele.08@itisdalmine.edu.it>
2026-03-26 09:42:17 +0100

    fatti getters setters e toString a classe Persona

--- indovinaChi/src/Persona.java
+++ indovinaChi/src/Persona.java
@@ -1,5 +1,7 @@
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Persona implements Serializable {
     @Serial
@@ -10,12 +12,108 @@ public class Persona implements Serializable {
     private ColoriÖch coloreOcchi;
     private ColoriPelle colorePelle;
     private boolean occhiali;
-    private boolean sesso;
+    private boolean sesso; //true = maschio; false = femmina
     private boolean capelliLunghi;
-    private boolean giovane;
-    private static String nomi;
+    private int eta;
+    private final static List<String> nomi = new ArrayList<>();
 
-    public Persona(String nome, ColoriCrapa cc, ColoriÖch co, ColoriPelle cp, boolean occhiali, boolean sesso, boolean capelliLunghi, boolean giovane) {
+    public Persona(String nome, int eta, ColoriCrapa cc, ColoriÖch co, ColoriPelle cp, boolean occhiali, boolean sesso, boolean capelliLunghi) {
+        setNome(nome);
+        setEta(eta);
+        setColoreCapelli(cc);
+        setColoreOcchi(co);
+        setColorePelle(cp);
+        setOcchiali(occhiali);
+        setSesso(sesso);
+        setCapelliLunghi(capelliLunghi);
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    private void setNome(String nome) {
+        if (nome == null) throw new NullPointerException("Il nome non può essere null");
+        nome = nome.trim().toLowerCase();
+        if (!nome.matches("[a-z]{3,15}")) throw new IllegalArgumentException("Formato nome non valido");
+        if (nomi.contains(nome)) throw new IllegalArgumentException("Nome già utilizzato");
+        this.nome = nome;
+        nomi.add(nome);
+    }
+
+    public ColoriCrapa getColoreCapelli() {
+        return coloreCapelli;
+    }
+
+    private void setColoreCapelli(ColoriCrapa coloreCapelli) {
+        if (coloreCapelli == null) throw new NullPointerException("Il colore dei capelli non può essere null");
+        this.coloreCapelli = coloreCapelli;
+    }
+
+    public ColoriÖch getColoreOcchi() {
+        return coloreOcchi;
+    }
+
+    private void setColoreOcchi(ColoriÖch coloreOcchi) {
+        if (coloreOcchi == null) throw new NullPointerException("Il colore degli occhi non può essere null");
+        this.coloreOcchi = coloreOcchi;
+    }
+
+    public ColoriPelle getColorePelle() {
+        return colorePelle;
+    }
+
+    private void setColorePelle(ColoriPelle colorePelle) {
+        if (colorePelle == null) throw new NullPointerException("Il colore della pelle non può essere null");
+        this.colorePelle = colorePelle;
+    }
+
+    public boolean isSesso() {
+        return sesso;
+    }
+
+    private void setSesso(boolean sesso) {
+        this.sesso = sesso;
+    }
+
+    public boolean isOcchiali() {
+        return occhiali;
+    }
+
+    private void setOcchiali(boolean occhiali) {
+        this.occhiali = occhiali;
+    }
+
+    public boolean isCapelliLunghi() {
+        return capelliLunghi;
+    }
+
+    private void setCapelliLunghi(boolean capelliLunghi) {
+        this.capelliLunghi = capelliLunghi;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+     void setEta(int eta) {
+        if (eta < 0) throw new IllegalArgumentException("L'età non può essere negativa");
+        this.eta = eta;
+    }
+
+    public static List<String> getNomi() {
+        return new ArrayList<>(nomi);
+    }
+
+    @Override
+    public String toString() {
+        String s = "occhiali: ";
+        if (occhiali) s += "si";
+        else s += "no";
+        if (sesso) s += ", sesso: maschio";
+        else s += ", sesso: femmina";
+        if (capelliLunghi) s += ", capelli: lunghi";
+        else s += ", capelli : corti";
+        return "nome: " + nome + ", età: " + eta + ", coloreCapelli: " + coloreCapelli + ", coloreOcchi: " + coloreOcchi + ", colorePelle: " + colorePelle + ", " + s;
     }
 }
